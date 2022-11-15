9c91a0398e09c6ce05bc8624b7db61556c743c64
Gwendal Le Bris <gwen.coding@gmail.com>
2022-11-15 14:23:22 +0100

    CRUD rails

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+
+    redirect_to(task_path(@task))
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to(task_path(@task))
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to(tasks_path, status: :see_other)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
